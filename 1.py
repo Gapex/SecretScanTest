51362d304e0e1e52d49e2d841140fda15ef2ec7e
Gapex <gapex@qq.com>
2026-01-14 22:24:04 +0800

    upload tests

--- 1.py
+++ 1.py
@@ -2,3 +2,8 @@ import os
 
 password="2341sldfosaf@l2308234"
 github_token="ghp_87654321abcdefghijklmnopqrstuvwxyz"
+# 触发 GitHub Secret Scanning 告警
+github_token = "ghp_87654321abcdefghijklmnopqrstuvwxyz1234567890"
+# 旧版格式（gho_/ghu_/ghs_ 开头也会触发）
+github_old_token = "gho_1234567890abcdefghijklmnopqrstuvwxyz1234"
+
