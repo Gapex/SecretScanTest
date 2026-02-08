f3cd67b140fc21467435b025bf44ef715ad4d7a0
Gapex <gapex@qq.com>
2026-02-08 15:51:07 +0800

    sfs

--- 1.py
+++ 1.py
@@ -7,3 +7,4 @@ github_token = "ghp_87654321abcdefghijklmnopqrstuvwxyz1234567890"
 # 旧版格式（gho_/ghu_/ghs_ 开头也会触发）
 github_old_token = "gho_1234567890abcdefghijklmnopqrstuvwxyz1234"
 
+github_token="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"
