b298a09b3041ac80b94768657ddc90b486709280
Gapex <gapex@qq.com>
2026-02-08 16:38:15 +0800

    d

--- 2.js
+++
@@ -1,5 +0,0 @@
-// AWS Access Key ID（触发告警）
-const aws_access_key_id = "AKIA1234567890ABCDEF";
-// AWS Secret Access Key（触发告警）
-const aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
-
